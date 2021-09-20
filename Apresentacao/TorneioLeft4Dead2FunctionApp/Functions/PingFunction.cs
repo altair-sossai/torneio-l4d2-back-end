a20807f97421d99df4d6be7689d0662c6e468dda
Altair Sossai <altair_sossai@outlook.com>
2021-09-19 17:44:14 -0700

    PingFunction

---
+++ Apresentacao/TorneioLeft4Dead2FunctionApp/Functions/PingFunction.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using TorneioLeft4Dead2FunctionApp.Extensions;
+
+namespace TorneioLeft4Dead2FunctionApp.Functions
+{
+    public class PingFunction
+    {
+        private static readonly Guid InstanceId = Guid.NewGuid();
+
+        [Function(nameof(PingFunction) + "_" + nameof(Get))]
+        public async Task<HttpResponseData> Get([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "ping")] HttpRequestData httpRequest)
+        {
+            return await httpRequest.OkAsync(new
+            {
+                instanceId = InstanceId,
+                now = DateTime.Now
+            });
+        }
+    }
+}
